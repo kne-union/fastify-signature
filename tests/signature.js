e516752695808b4b18caa11c8d2ca1c8970b9f0d
Linzp <sunandmoon001@163.com>
2025-06-26 16:05:58 +0800

    修正接口

---
+++ tests/signature.js
@@ -0,0 +1,3 @@
+const generateSignature = require('../generateSignature');
+
+console.log(generateSignature('196203562894623744', '2a2eda5baef55aad31fc3cf63ce1c1d542c95b53aa7a433d569c75c20adc2ec3', 3 * 60));
